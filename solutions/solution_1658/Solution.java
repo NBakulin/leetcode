8131bff215af6f1872c9bb54317643329e39b869
Nikita Bakulin <nikita.bakulin@magicplan.app>
2023-09-26 02:43:24 +0300

    [SOLUTION-1658]

--- solutions/solution_1658/Solution.java
+++ solutions/solution_1658/Solution.java
@@ -3,22 +3,51 @@ package solutions.solution_1658;
 class Solution {
 
     public int minOperations(int[] nums, int x) {
-        int i = 0;
-        int j = nums.length - 1;
+        int sum = -x;
+        int n = nums.length;
+
+        for (int num: nums) {
+            sum+= num;
+        }
+        if (sum == 0) {
+            return n;
+        }
+        if (sum < 0) {
+            return -1;
+        }
+
+        sum = 0;
+        int currentSum;
         int result = 0;
+        int currentResult;
+        int finalResult = -1;
+        int leftIndex;
+        int rightIndex = 0;
 
-        while (i <= j) {
-            x -= nums[i] <= nums[j] ? nums[i++] : nums[j--];
-            result++;
-            if (x <= 0) {
-                break;
+
+        for (int right: nums) {
+            leftIndex = n - 1;
+            currentSum = sum;
+            currentResult = result;
+
+            while (currentSum < x && rightIndex <= leftIndex) {
+                currentSum += nums[leftIndex--];
+                currentResult++;
             }
+
+            if (currentSum == x) {
+                if (finalResult == -1) {
+                    finalResult = currentResult;
+                } else {
+                    finalResult = Math.min(finalResult, currentResult);
+                }
+            }
+
+            result++;
+            sum += right;
+            rightIndex++;
         }
 
-        if (x == 0) {
-            return result;
-        }
-
-        return -1;
+        return finalResult;
     }
 }
