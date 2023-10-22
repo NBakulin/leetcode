f50cc9d07efc0b21c292edf9be2c796944a20262
Nikita Bakulin <nikita.bakulin@magicplan.app>
2023-10-22 22:08:09 +0300

    [Array][Dynamic Programming][Queue][Sliding Window][Heap (Priority Queue)][Monotonic Queue] Constrained Subsequence Sum

--- solutions/solution_1425/Solution.java
+++ solutions/solution_1425/Solution.java
@@ -1,60 +1,34 @@
 package solutions.solution_1425;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// This is a solution provided by LeetCode. It's beautiful...
 class Solution {
-    int[] nums;
-    int maxSingleValue = (int)-1e6 - 1;
-    int maxValue = (int)-1e9 - 1;
-    int[] dp;
     public int constrainedSubsetSum(int[] nums, int k) {
-        this.nums = nums;
-        this.dp = new int[nums.length];
-        getMaxSum(0, k);
+        Deque<Integer> queue = new ArrayDeque<>();
+        int dp[] = new int[nums.length];
 
-        if (this.maxValue == 0) return this.maxSingleValue;
-
-        return Math.max(this.maxValue, this.maxSingleValue);
-    }
-
-    public int getMaxSum(int currentIndex, int k) {
-        if (currentIndex > this.nums.length - 1) {
-            return 0;
-        }
-
-        int currentValue = this.nums[currentIndex];
-        this.maxSingleValue = Math.max(this.maxSingleValue, currentValue);
-        if (currentIndex == this.nums.length - 1) {
-            this.dp[currentIndex] = currentValue;
-            return currentValue;
-        }
-
-
-        int cachedValue = this.dp[currentIndex];
-        if (cachedValue != 0) return Math.max(cachedValue, 0);
-
-        int newMaxValue = Integer.MIN_VALUE;
-        int maxValueIndex = currentIndex + 1;
-        for (int i = currentIndex + 1; i <= currentIndex + k && i < this.nums.length; i++) {
-            int newValue = this.nums[i];
-            if (newValue >= newMaxValue) {
-                newMaxValue = newValue;
-                maxValueIndex = i;
+        for (int i = 0; i < nums.length; i++) {
+            if (!queue.isEmpty() && i - queue.peek() > k) {
+                queue.poll();
             }
-            if (newMaxValue > 0) break;
-        }
 
-        int result = 0;
-        if (newMaxValue >= 0) {
-            result = Math.max(this.getMaxSum(maxValueIndex, k), result);
-        } else {
-            for (int i = maxValueIndex; i <= currentIndex + k; i++) {
-                result = Math.max(getMaxSum(i, k), result);
+            dp[i] = (!queue.isEmpty() ? dp[queue.peek()] : 0) + nums[i];
+            while (!queue.isEmpty() && dp[queue.peekLast()] < dp[i]) {
+                queue.pollLast();
+            }
+
+            if (dp[i] > 0) {
+                queue.offer(i);
             }
         }
 
-        result += currentValue;
-        maxValue = Math.max(maxValue, result);
-        this.dp[currentIndex] = result;
+        int ans = Integer.MIN_VALUE;
+        for (int num : dp) {
+            ans = Math.max(ans, num);
+        }
 
-        return result;
+        return ans;
     }
 }
