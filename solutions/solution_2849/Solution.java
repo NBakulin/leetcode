8bb8e68f0b1032b03bcdd60779fd9fa78ff2391d
Nikita Bakulin <nikita.bakulin@magicplan.app>
2023-11-08 12:19:52 +0200

    [Math] 2849. Determine if a Cell Is Reachable at a Given Time

---
+++ solutions/solution_2849/Solution.java
@@ -0,0 +1,8 @@
+package solutions.solution_2849;
+
+class Solution {
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if (sx == fx && sy == fy) return t != 1;
+        return Math.max(Math.abs(sx - fx), Math.abs(sy - fy)) <= t;
+    }
+}
