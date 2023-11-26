b3f73f835d74fbed2b8618aa3496ab57951437ea
Nikita Bakulin <nikita.bakulin@magicplan.app>
2023-11-26 14:12:19 +0200

    [Array][Greedy][Sorting][Matrix] 1727. Largest Submatrix With Rearrangements

---
+++ solutions/solution_1727/Solution.java
@@ -0,0 +1,27 @@
+package solutions.solution_1727;
+
+import java.util.Arrays;
+
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int result = 0;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (matrix[row][col] != 0 && row > 0) {
+                    matrix[row][col] += matrix[row - 1][col];
+                }
+            }
+
+            int[] currRow = matrix[row].clone();
+            Arrays.sort(currRow);
+            for (int i = 0; i < n; i++) {
+                result = Math.max(result, currRow[i] * (n - i));
+            }
+        }
+
+        return result;
+    }
+}
