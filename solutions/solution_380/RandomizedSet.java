83bc2b54a70730b67a4dcc12e6a0168e6d03c6d4
Nikita Bakulin <nikita.bakulin@magicplan.app>
2024-01-16 21:47:42 +0500

    [Array][Hash Table][Math][Design][Randomized] 380. Insert Delete GetRandom O(1)

---
+++ solutions/solution_380/RandomizedSet.java
@@ -0,0 +1,51 @@
+package solutions.solution_380;
+
+import java.util.*;
+
+class RandomizedSet {
+
+    public Map<Integer, Integer> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
+
+    public RandomizedSet() {
+
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        map.put(val, list.size());
+        list.add(val);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+        list.set(index, list.get(list.size() - 1));
+        map.put(list.get(index), index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
